2e9f45b59bae8cf8c6ccc23d561ebb48382b53bc
Max Shevchencko <tier4ik@gmail.com>
2020-09-22 12:07:55 +0400

    Added filter and pagination functions. Added frontend testing functionality.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,31 @@
 module.exports = {
     async feed(parent, args, context) {
-        return await context.prisma.link.findMany()
+
+        const where = args.filter ? {
+            OR: [
+                { description: { contains: args.filter } },
+                { url: { contains: args.filter } }
+            ]
+        } : {};
+
+        const links = await context.prisma.link.findMany({
+            where,
+            skip: args.skip,
+            take: args.take
+        });
+
+        const count = await context.prisma.link.count({ where });
+
+        return {
+            links,
+            count
+        }
     },
+    async link(parent, args, context) {
+        return await context.prisma.link.findOne({
+            where: {
+                id: parseInt(args.id)
+            }
+        })
+    }
 }
